95d0a873a0b671851bbbc0db98d404a3f918d25b
Jonathan Mark Eskreis-Winkler <eskreiswinkler@aitken-cluster.uchicago.edu>
2015-11-20 10:23:49 -0600

    a

--- Code/Irish_Wind/run_file.sh
+++ Code/Irish_Wind/run_file.sh
@@ -10,7 +10,7 @@
 . /etc/profile
 #module load packages/matlab/r2013a
 
-matlab -nodisplay -nojvm < test.m
+matlab -nodisplay -nojvm < hyper_param_search.m
 
 echo "Code has finished running." | mail -s "Alert" "jeskreiswinkler@gmail.com"
 
