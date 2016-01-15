a9e45770de7f890b32e76286d18ba4626fc11941
eskreiswinkler <eskreiswinkler@uchicago.edu>
2016-01-14 23:42:51 -0600

    Constructed demos for GPR in 1 and 2 dims

--- Code/Demos/GP_1D.m
+++ Code/Demos/GP_1D.m
@@ -1,36 +1,80 @@
+function[] = GP_1D(params)
+params.num_dp = 700;
+params.bdry = 7;
+params.noise_factor = 10;
+params.n_iters = 3;
+params.n_restarts = 20;
+params.t_o = [5 0.5 0.5];
+params.k = 2;
+params.ndensestages = 1;
+params.fraction = 0.6;
+params.maxclustersize = 100;
+params.bypass=3;
+params.minclustersize = 1;
+
 addpath('../GP_functions/')
+% Now link to the location of the MMF code for MMFC
+addpath('~/MMF_project/mmfc/v4/src/matlab/')
 
-num_datapoints = 200;
-x = linspace(-3,3,num_datapoints);
 
-m = cos(2*x)+x;
-% True function
-a=zeros(3,1);
+x = linspace(-params.bdry,params.bdry,params.num_dp);
+% Define a function here
+%m = -20*(x<=0)+20*(x>=0);
+m = 5*cos(2*x)+x.^2;
+
+% The received data $y$ is the true function with noise.
+y = m + randn(size(m))*params.noise_factor;
+
+% Plot true function and noisy received signal.
 a(1) = plot(x,m,'LineWidth',4);
-% Add noise
-factor = 1.2;
-y = m + randn(size(m))*factor;
 hold on
 a(2) = plot(x,y,'o');
 hold off
 legend('True function','Data')
 
 
+store.th = zeros(params.n_restarts,3);
+store.th_mmf = zeros(params.n_restarts,3);
+store.t_o = zeros(params.n_restarts,3);
 
-num_steps = 30;
-% this example considers an RBF kernel;
-t_o = [2 0.25 0.5];
-miter=num_steps;
-[theta_fmin, fval] = fminsearch(@(theta) LL_calc(theta,x,y), t_o,optimset('Display','iter','MaxIter',miter));
+for i = 1:params.n_restarts
+    t_cur = -1*ones(size(params.t_o));
+    while sum(t_cur<0)>0
+    	t_cur = params.t_o + randn(1,3).*params.t_o*1.5;
+    end
+    store.t_o(i,:) = t_cur;
+    [th_mmf, fval_mmf] = fminsearch(@(theta) LL_calc(theta,x,y,1,params), t_cur,optimset('Display','iter','MaxIter',params.n_iters));
+    [th, fval] = fminsearch(@(theta) LL_calc(theta,x,y,0,params), t_cur,optimset('Display','iter','MaxIter',params.n_iters));
+    if sum(th_mmf<0)>0
+        store.th_mmf(i,:) = nan(size(params.t_o));
+    else
+    	store.th_mmf(i,:) = th_mmf;
+    end
+    if sum(th<0)>0
+        store.th(i,:) = nan(size(params.t_o));
+    else
+        store.th(i,:) = th;
+    end
+end
 
-grid = linspace(-3,3,round(num_datapoints*0.9));
-K = make_rbf(x,theta_fmin);
-K_star = make_rbf(x,theta_fmin,grid);
+th_mmf = nanmean(store.th_mmf,1);
+th = nanmean(store.th,1);
+
+grid = linspace(-params.bdry,params.bdry,round(params.num_dp*0.75));
+K = make_rbf(x,th);
+K_mmf = make_rbf(x, th_mmf);
+K_star = make_rbf(x,th,grid);
+K_star_mmf = make_rbf(x,th_mmf,grid);
 B = K\y';
+B_mmf = K_mmf\y';
 y_pred = K_star'*B;
+y_pred_mmf = K_star_mmf'*B;
 
 hold on
-a(3) = plot(grid,y_pred,'LineWidth',4)
+a(3) = plot(grid,y_pred,'LineWidth',4);
+a(4) = plot(grid,y_pred_mmf,'LineWidth',4);
 hold off
 
-legend(a,'Truth','Noisy','GPR')
+legend(a,'Truth','Noisy','GPR','GPR MMF')
+
+print('GP_1D','-djpeg','-noui')
--- Code/Demos/GP_2D.m
+++ Code/Demos/GP_2D.m
@@ -1,22 +1,35 @@
+%function[] = GP_2D(params)
+params.num_dp = 25;
+params.bdry = 7;
+params.noise_factor = 1;
+params.n_iters = 3;
+params.n_restarts = 5;
+params.t_o = [5 0.25 0.5 0.5];
+params.k = 2;
+params.ndensestages = 1;
+params.fraction = 0.6;
+params.maxclustersize = 100;
+params.bypass=3;
+params.minclustersize = 1;
+
 addpath('../GP_functions/')
+% Now link to the location of the MMF code for MMFC
+addpath('~/MMF_project/mmfc/v4/src/matlab/')
 
-num_dp = 25;
-end_pt = 3;
-x = linspace(-end_pt,end_pt,num_dp);
+x = linspace(-params.bdry,params.bdry,params.num_dp);
 
-d = [repelem(x,num_dp); repmat(x,1,num_dp)]';
-m = zeros(num_dp);
-e = zeros(num_dp^2,1);
+d = [repelem(x,params.num_dp); repmat(x,1,params.num_dp)]';
+m = zeros(params.num_dp);
+e = zeros(params.num_dp^2,1);
 
-for col = 1:num_dp
-    for row = 1:num_dp
+for col = 1:params.num_dp
+    for row = 1:params.num_dp
         m(col,row) = cos(x(col))*x(col)*sin(x(row));
-        e( (col-1)*num_dp+row) = m(col,row);
+        e( (col-1)*params.num_dp+row) = m(col,row);
     end
 end
 
-factor = 0.5;
-y = m+randn(size(m))*factor;
+y = m+randn(size(m))*params.noise_factor;
 
 % Observe the effect of the noise and assess the data being modelled.
 figure(1)
@@ -25,49 +38,87 @@ hold on
 surf(x,x,y)
 hold off
 
+print('GP_2Da','-djpeg','-noui')
+
 figure(2)
-for cur_plot = 1:num_dp
-    subplot(5,5,cur_plot)
-    plot(x,m(cur_plot,:))
+a = zeros(4,1);
+for cur_plot = 1:params.num_dp
+    subplot(sqrt(params.num_dp),sqrt(params.num_dp),cur_plot)
+    a(1) = plot(x,m(cur_plot,:));
     hold on
-    plot(x,y(cur_plot,:),'o')
+    a(2) = plot(x,y(cur_plot,:),'o');
     hold off
 end
 
+print('GP_2Db','-djpeg','-noui')
 
 
-num_steps = 30;
-% this example considers an RBF kernel;
-t_o = [2 0.25 0.5 0.5];
-miter=num_steps;
-[theta_fmin, fval] = fminsearch(@(theta) LL_calc(theta',d',e'), t_o,optimset('Display','iter','MaxIter',miter));
+store.th = zeros(params.n_restarts,length(params.t_o));
+store.th_mmf = zeros(params.n_restarts,length(params.t_o));
+store.t_o = zeros(params.n_restarts,length(params.t_o));
 
-g_num_dp = round(num_dp*0.9);
-g = linspace(-end_pt,end_pt,g_num_dp);
+
+for i = 1:params.n_restarts
+    t_cur = -1*ones(size(params.t_o));
+    while sum(t_cur<0)>0
+    	t_cur = params.t_o + randn(1,length(params.t_o)).*params.t_o*1;
+    end
+    store.t_o(i,:) = t_cur;
+    [th_mmf, fval_mmf] = fminsearch(@(theta) LL_calc(theta',d',e',1,params), t_cur,optimset('Display','iter','MaxIter',params.n_iters));
+    [th, fval] = fminsearch(@(theta) LL_calc(theta',d',e',0,params), t_cur,optimset('Display','iter','MaxIter',params.n_iters));
+    if sum(th_mmf<0)>0
+        store.th_mmf(i,:) = nan(size(params.t_o));
+    else
+    	store.th_mmf(i,:) = th_mmf;
+    end
+    if sum(th<0)>0
+        store.th(i,:) = nan(size(params.t_o));
+    else
+        store.th(i,:) = th;
+    end
+end
+th_mmf = nanmean(store.th_mmf,1);
+th = nanmean(store.th,1);
+
+g_num_dp = round(params.num_dp*0.75);
+g = linspace(-params.bdry,params.bdry,g_num_dp);
 g_d = [repelem(g,g_num_dp); repmat(g,1,g_num_dp)];
-K = make_rbf(d',theta_fmin');
-K_star = make_rbf(d,theta_fmin,g_d);
+K = make_rbf(d',th');
+K_mmf = make_rbf(d',th_mmf');
+K_star = make_rbf(d',th',g_d);
+K_star_mmf = make_rbf(d',th_mmf',g_d);
 B = K\e;
+B_mmf = K_mmf\e;
 e_pred = K_star'*B;
+e_pred_mmf = K_star_mmf'*B;
 
 % make e_pred into a y size matrix
 y_pred = zeros(g_num_dp);
+y_pred_mmf = y_pred;
 for col = 1:g_num_dp
     for row = 1:g_num_dp
         y_pred(col,row) = e_pred((col-1)*g_num_dp+row);
+        y_pred_mmf(col,row) = e_pred_mmf((col-1)*g_num_dp+row);
     end
 end
 
 figure(1)
 hold on
 surf(g,g,y_pred)
+surf(g,g,y_pred_mmf);
 hold off
+print('GP_2Da','-djpeg','-noui')
 
 figure(2)
 % hard to compare in this direction since the grid is a little different.
 for cur_plot = 1:g_num_dp
-    subplot(5,5,cur_plot)
+    subplot(sqrt(params.num_dp),sqrt(params.num_dp),cur_plot)
     hold on
-    plot(g,y_pred(cur_plot,:))
+    a(3) = plot(g,y_pred(cur_plot,:),'LineWidth',2);
+    a(4) = plot(g,y_pred_mmf(cur_plot,:),'LineWidth',2);
     hold off
+    if cur_plot == g_num_dp
+        legend(a,'Truth','Data','GP','GP MMF')
+    end
 end
+print('GP_2Db','-djpeg','-noui')
