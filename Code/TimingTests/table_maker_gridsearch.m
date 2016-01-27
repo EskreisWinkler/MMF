f1057a6a61c0e71d3a6cad609d009145d77d3367
eskreiswinkler <eskreiswinkler@uchicago.edu>
2016-01-27 12:39:40 -0600

    Moving Day Jan27

---
+++ Code/TimingTests/table_maker_gridsearch.m
@@ -0,0 +1,49 @@
+addpath('../GP_functions/')
+%addpath('~/MMF_project/mmfc/v4/src/matlab/')
+addpath('~/galton_home/mmfc/v4/src/matlab/')
+% Set up to imitate the test run by ONeil
+
+n = [1e4 2e4 5e4 1e5 2e5 5e5 1e6]';
+n = n/250;
+
+grid_var = 'fraction';
+
+table.assemb = zeros(size(n));
+table.factor = table.assemb;
+table.solve = table.factor;
+table.det = table.solve;
+table.error = table.det;
+
+params = GP_params();
+grid = [0.1 0.3 0.5 0.7 0.9];
+table_store = cell(length(grid),1);
+for grid_i = 1:length(frac_vals)
+    params.fraction = grid(grid_i);
+    for ind = 1:length(n)
+        disp(ind)
+        cur_n = n(ind);
+        x = linspace(-params.bdry,params.bdry,cur_n);
+        tic()
+        K = make_rbf(x,params.t_o_1D);
+        table.assemb(ind) = toc();
+        tic()
+        K_mmf = MMF(K, params);
+        table.factor(ind) = toc();
+        tic()
+        d = K_mmf.determinant();
+        table.det(ind) = toc();
+        K_mmf_inv = K_mmf;
+        tic()
+        K_mmf_inv.invert()
+        table.solve(ind) = toc();
+        table.error(ind) = K_mmf.froberror();
+    end
+    table_store{grid_i} = table;
+end
+
+vars = {'Assembly Time','Factoring Time','Inversion Time','Determinant Computation Time','Matrix Error'};
+
+d = 1;
+file_name = sprintf('gstore_%s_%d',grid_var,d);
+save(file_name,'vars','table','n')
+
