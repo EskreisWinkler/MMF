b277caf7564fbbb9366f84c1c9893a48f2d8f511
Jonathan Mark Eskreis-Winkler <eskreiswinkler@aitken-cluster.uchicago.edu>
2016-05-04 15:00:14 -0500

    MD 5.4

--- SSL/Benchmarks/run_file_secstr.sh
+++ SSL/Benchmarks/run_file_secstr.sh
@@ -3,14 +3,14 @@
 #$ -cwd
 ##$ -j y
 #$ -S /bin/bash
-#$ -o output/out.txt
-#$ -e output/err.txt
+#$ -o output/out_secstr.txt
+#$ -e output/err_secstr.txt
 ##$ -t 1-10
 #
 . /etc/profile
 #module load packages/matlab/r2013a
 
-matlab -nodisplay -nojvm -r 'MMF_comparisons(1,5); exit;'
+matlab -nodisplay -nojvm -r 'MMF_comparisons_secstr(5,3,20); exit;'
 
 echo "Code has finished running." | mail -s "Alert" "jeskreiswinkler@gmail.com"
 
