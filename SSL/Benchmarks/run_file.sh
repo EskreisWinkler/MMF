bf51949a050cd26310bb7559a3b3e78cd77770e6
Jonathan Mark Eskreis-Winkler <eskreiswinkler@aitken-cluster.uchicago.edu>
2016-05-04 22:32:51 -0500

    MD 5.4

--- SSL/Benchmarks/run_file.sh
+++ SSL/Benchmarks/run_file.sh
@@ -10,7 +10,7 @@
 . /etc/profile
 #module load packages/matlab/r2013a
 
-matlab -nodisplay -nojvm -r 'MMF_comparisons(1,5); exit;'
+matlab -nodisplay -nojvm -r 'MMF_comparisons(1,6); exit;'
 
 echo "Code has finished running." | mail -s "Alert" "jeskreiswinkler@gmail.com"
 
