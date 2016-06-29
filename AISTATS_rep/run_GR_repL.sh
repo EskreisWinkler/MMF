86727cdc471a8b2b0054f9e65cb53953f0ef58ce
EskreisWinkler <eskreiswinkler@uchicago.edu>
2016-06-29 10:34:57 -0500

    6.29

--- AISTATS_rep/run_GR_repL.sh
+++ AISTATS_rep/run_GR_repL.sh
@@ -10,7 +10,7 @@
 . /etc/profile
 #module load packages/matlab/r2013a
 
-matlab -nodisplay -nojvm -r 'GR_rep; exit;'
+matlab -nodisplay -nojvm -r 'GR_repL; exit;'
 
 echo "Code has finished running." | mail -s "Alert" "jeskreiswinkler@gmail.com"
 
