ce63cdbdc530716977fe5c83e7bf558f82a974c4
EskreisWinkler <eskreiswinkler@uchicago.edu>
2016-06-30 20:00:21 -0500

    6.30

---
+++ AISTATS_rep/Gnu_rep_analysis.m
@@ -0,0 +1,14 @@
+addpath_ssl(0) % change
+
+load Data/Gnu_rep_fro.mat
+
+data = csvread('Data/p2p-Gnutella06.csv');
+p = SSL_params(1,1);
+c.num_edges = size(data,1);
+
+data = data(randperm(c.num_edges),:);
+
+c.node_ids = sort(unique([data(:,1); data(:,2)]));
+c.num_nodes = length(c.node_ids);
+
+plot(c.num_nodes*(1-core_vec),err_store_normed,'ro-')
