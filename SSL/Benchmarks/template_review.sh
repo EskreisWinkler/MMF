3b1b2c9f9f4c23b4a808b97c18db1ce6d542b552
Aitken <jeskreiswinkler@gmail.com>
2016-07-08 00:02:52 -0500

    7.7

--- SSL/Benchmarks/template_review.sh
+++ SSL/Benchmarks/template_review.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=review_dataset%1%_laptype%2%_draws%3%
 #SBATCH --output=/home/eskreiswinkler/scratch-midway/review/review_data%1%_laptype%2%_draws%3%.out
 #SBATCH --error=/home/eskreiswinkler/scratch-midway/review/review_data%1%_laptype%2%_draws%3%.err
-#SBATCH --time=%3%:00:00
+#SBATCH --time=10:00:00
 #SBATCH --partition=sandyb
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=12
