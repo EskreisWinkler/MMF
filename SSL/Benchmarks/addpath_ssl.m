fded0e4b6a2f2dde4a8c2996432470922a48dc05
EskreisWinkler <eskreiswinkler@uchicago.edu>
2016-07-10 16:58:37 -0500

    7.10

--- SSL/Benchmarks/addpath_ssl.m
+++ SSL/Benchmarks/addpath_ssl.m
@@ -1,16 +1,16 @@
 function[] = addpath_ssl(server)
 if server == 0 %home
     risi = '/Users/jeskreiswinkler/Drive/15fall/Kondor/';
-    mmf = '/Users/jeskreiswinkler/mmfc/v4/src/matlab';
+    mmf = '/Users/jeskreiswinkler/mmfc/release1.0/pMMF/matlab/';
 elseif server == 1; % aitken
     risi = '/net/wallace/ga/eskreiswinkler/MMF/';
-    mmf = '/net/wallace/ga/eskreiswinkler/mmfc/v4/src/matlab';
+    mmf = '/net/wallace/ga/eskreiswinkler/mmfc/v4/src/matlab/';
 elseif server==2 %rcc
     risi = '/home/eskreiswinkler/MMF/';
-    mmf = '/home/eskreiswinkler/mmfc/v4/src/matlab';
+    mmf = '/home/eskreiswinkler/mmfc/release1.0/pMMF/matlab/';
 end
 
-addpath(sprintf('%s/SSL/Buffalo/',risi),sprintf('%s/SSL/Benchmarks/',risi));
-addpath(sprintf('%s/GP/GP_param_search/',risi),sprintf('%s/GP/GP_functions/',risi));
-addpath(sprintf('%s/SSL/SSL_scripts/',risi))
-addpath(sprintf('%s',mmf));
+addpath(sprintf('%sSSL/Buffalo/',risi),sprintf('%s/SSL/Benchmarks/',risi));
+addpath(sprintf('%sGP/GP_param_search/',risi),sprintf('%s/GP/GP_functions/',risi));
+addpath(sprintf('%sSSL/SSL_scripts/',risi))
+addpath(sprintf('%s/',mmf));
